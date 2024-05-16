4357bbe7ab8bae9b7aee9236fa848f72507b0312
Tushar Sharma <tusharsharma@Tushars-MacBook-Air-2.local>
2024-05-16 19:06:02 +0530

    Added the models

---
+++ src/model/User.ts
@@ -0,0 +1,76 @@
+import mongoose, {Schema, Document} from "mongoose";
+
+
+//Interface making for type safety in TypeScript
+
+export interface Message extends Document {
+    content: string;
+    createdAt: Date;
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+})
+
+export interface User extends Document {
+    username: string;
+    email: string;
+    password: string;
+    verifyCode: string,
+    verifyCodeExpiry: Date;
+    isVerified: boolean;
+    isAcceptingMessages: boolean;
+    messages: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+    username:{
+        type: String,
+        required: [true, "Username is required"],
+        trim: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+\@.+\..+/, 'Please use a valid email address']
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    },
+    verifyCode: {
+        type: String,
+        required: [true, "Verify Code is required"]
+    },
+    verifyCodeExpiry: {
+        type: Date,
+        required: [true, "Verify Code Expiry is required"]
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    isAcceptingMessages: {
+        type: Boolean,
+        default: true
+    },
+    messages: [MessageSchema]
+})
+
+
+//NextJs is unlike Express where the Mongoose makes the database on MongoDb for only once,
+//Here we have to see that in past whether the model is made, if not we will make it for the very first time
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema)
+
+export default UserModel;
