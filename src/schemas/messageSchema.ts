421eabf0318b396d9ceaac6cebcdd8964d3174e2
sharmatushar14 <sharmatusharofficial14@gmail.com>
2024-05-16 22:47:17 +0530

    Added zod for validation

---
+++ src/schemas/messageSchema.ts
@@ -0,0 +1,6 @@
+import {z} from 'zod'
+
+export const messageSchema = z.object({
+    content: z.string().min(10, "Content must be atleast 10 characters long")
+    .max(300, {message: "Content must not be longer than 300 characters"})
+})
